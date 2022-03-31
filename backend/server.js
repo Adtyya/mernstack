0e79b3d7858554c2e88ae0e47eeb60484768ea34
aditya <aditdualapan@gmail.com>
2022-03-31 10:09:02 +0700

    up

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,27 @@ const userRoutes = require("./routes/userRoutes");
 const notesRoutes = require("./routes/notesRoutes");
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running ...");
-});
-// app.get("/api/notes", (req, res) => {
-//   res.json(notes);
-// });
-
 app.use("/api/user", userRoutes);
 app.use("/api/notes", notesRoutes);
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is running ...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
