22ac7c8403d7ecd55199c6d9efe60152b3c20a2c
aditya <aditdualapan@gmail.com>
2022-03-31 10:28:15 +0700

    First

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,17 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/notes", notesRoutes);
 
-__dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is running ...");
-  });
-}
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// } else {
+// }
+app.get("/", (req, res) => {
+  res.send("Server is running ...");
+});
 
 app.use(notFound);
 app.use(errorHandler);
